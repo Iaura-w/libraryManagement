9f8b1774cdaf308c62c184eb8b52402a15358d4f
Iaura-w <laura.wo19@gmail.com>
2024-06-29 12:28:03 +0200

    update database settings, add dependencies

--- init-mongo.js
+++ init-mongo.js
@@ -23,24 +23,28 @@ db.books.insertMany([
         title: "Hamlet",
         author: "William Shakespeare",
         available: true,
+        isbn: 12345,
         loanHistory: []
     },
     {
         title: "War and Peace",
         author: "Leo Tolstoy",
         available: true,
+        isbn: 24251,
         loanHistory: []
     },
     {
         title: "The Odyssey",
         author: "Homer",
         available: true,
+        isbn: 33441,
         loanHistory: []
     },
     {
         title: "1984",
         author: "George Orwell",
         available: true,
+        isbn: 67564,
         loanHistory: []
     }
 ]);
